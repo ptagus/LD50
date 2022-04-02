73943dff0b6ab2b17d682d3972f08e30534b5894
ptagus <ptagusss@ya.ru>
2022-04-03 00:31:02 +0300

    Funny

--- Assets/Scripts/TestAnim.cs
+++ Assets/Scripts/TestAnim.cs
@@ -5,20 +5,9 @@ using DragonBones;
 
 public class TestAnim : MonoBehaviour
 {
-    void Start()
+    private void Update()
     {
-        UnityFactory.factory.LoadDragonBonesData("NewProject_1_ske"); // DragonBones file path (without suffix)
-        UnityFactory.factory.LoadTextureAtlasData("NewProject_1_tex"); //Texture atlas file path (without suffix) 
-
-        // Create armature.
-        var armatureComponent = UnityFactory.factory.BuildArmatureComponent("SimpleGuy");
-        // Input armature name
-
-        // Play animation.
-        armatureComponent.animation.Play("animtion0");
-
-        // Change armatureposition.
-        armatureComponent.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        transform.Rotate(0, 0, 1);
     }
 }
 
