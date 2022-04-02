33104b004bd22b03738705c8b71ee68e62931286
Даниил Захаревич <zaharevich.d@panoramik.ru>
2022-04-03 01:19:04 +0300

    ebanaya_pomoika

--- Assets/Scripts/StartTimer.cs
+++ Assets/Scripts/StartTimer.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class StartTimer : MonoBehaviour
 {
-    public GameObject number3;
-    public GameObject number2;
-    public GameObject number1;
-    public GameObject run;
+    public GameObject panelNumber;
+    public GameObject currentNumber;
+    public GameObject[] numbersMassive;
+    int curNumberInMassive = 0;
+    bool readyToChange = false;
+   
+   // bool readyToChange = false;
     // Start is called before the first frame update
     void Start()
     {
+
+        currentNumber = numbersMassive[curNumberInMassive];
+        ChangeNumber();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((readyToChange == true) && (curNumberInMassive <4))
+        {
+            readyToChange = false;
+            Debug.Log(readyToChange);
+            Debug.Log(curNumberInMassive);
+            ChangeNumber();
+        }
     }
 
+     void ChangeNumber()
+    {
+        currentNumber = numbersMassive[curNumberInMassive++];
+        panelNumber.GetComponent<SpriteRenderer>().sprite = currentNumber.GetComponent<SpriteRenderer>().sprite;
+        StartCoroutine(TimerCoroutine());
+        
+        
+    }
+   IEnumerator TimerCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        readyToChange = true;
+        Debug.Log(readyToChange);
+    }
 }
