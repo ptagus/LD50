df5413f23d7203a143a53c5de3d576d21d6a2771
Даниил Захаревич <zaharevich.d@panoramik.ru>
2022-04-02 17:00:16 +0300

    CreateTimerScript

---
+++ Assets/Scripts/StartTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTimer : MonoBehaviour
+{
+    public GameObject number3;
+    public GameObject number2;
+    public GameObject number1;
+    public GameObject run;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
